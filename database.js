49d1d401f3652a5820341f748effed243ae18b9a
Pamidu <routerunlockking@gmail.com>
2026-03-25 18:07:26 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     }
 
     try {
-        const uri = process.env.MONGO_URI || 'mongodb+srv://test:Abc%4012345@cluster0.a77j2uu.mongodb.net/?appName=Cluster0';
+        const uri = process.env.MONGO_URI || 'mongodb+srv://ravimotors:10669Abc@cluster0.mqdicae.mongodb.net/testdb?retryWrites=true&w=majority';
         const db = await mongoose.connect(uri, {
             serverSelectionTimeoutMS: 5000 // Tweak timeout down so Serverless fails faster instead of hanging
         });
